440c1f8b713015a21e1874e590a58e3c5f4c6a9c
Mohammad Yusuf Syafroni Karim <syafroni@aol.com>
2025-05-21 18:59:11 +0700

    feat: Update model URLs in Config.py to use new endpoint

--- Writer/Config.py
+++ Writer/Config.py
@@ -1,45 +1,31 @@
-INITIAL_OUTLINE_WRITER_MODEL = (
-    "ollama://gemma3:12b@10.23.82.116"  # Note this value is overridden by the argparser
-)
-CHAPTER_OUTLINE_WRITER_MODEL = (
-    "ollama://gemma3:12b@10.23.82.116"  # Note this value is overridden by the argparser
-)
-CHAPTER_STAGE1_WRITER_MODEL = (
-    "ollama://gemma3:12b@10.23.82.116"  # Note this value is overridden by the argparser
-)
-CHAPTER_STAGE2_WRITER_MODEL = (
-    "ollama://gemma3:12b@10.23.82.116"  # Note this value is overridden by the argparser
-)
-CHAPTER_STAGE3_WRITER_MODEL = (
-    "ollama://gemma3:12b@10.23.82.116"  # Note this value is overridden by the argparser
-)
-FINAL_NOVEL_EDITOR_MODEL = "ollama://gemma3:12b@10.23.82.116"  # Model for the final novel-wide edit pass (used by NovelEditor.py) # Note this value is overridden by the argparser
-CHAPTER_REVISION_WRITER_MODEL = (
-    "ollama://gemma3:12b@10.23.82.116"  # Note this value is overridden by the argparser
-)
-REVISION_MODEL = (
-    "ollama://gemma3:12b@10.23.82.116"  # Note this value is overridden by the argparser
-)
-EVAL_MODEL = (
-    "ollama://gemma3:12b@10.23.82.116"  # Note this value is overridden by the argparser
-)
-INFO_MODEL = (
-    "ollama://gemma3:12b@10.23.82.116"  # Note this value is overridden by the argparser
-)
-SCRUB_MODEL = (
-    "ollama://gemma3:12b@10.23.82.116"  # Note this value is overridden by the argparser
-)
-CHECKER_MODEL = "ollama://gemma3:12b@10.23.82.116"  # Model used to check results
-TRANSLATOR_MODEL = "ollama://gemma3:12b@10.23.82.116"
+INITIAL_OUTLINE_WRITER_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Note this value is overridden by the argparser
+CHAPTER_OUTLINE_WRITER_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Note this value is overridden by the argparser
+CHAPTER_STAGE1_WRITER_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Note this value is overridden by the argparser
+CHAPTER_STAGE2_WRITER_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Note this value is overridden by the argparser
+CHAPTER_STAGE3_WRITER_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Note this value is overridden by the argparser
+FINAL_NOVEL_EDITOR_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Model for the final novel-wide edit pass (used by NovelEditor.py) # Note this value is overridden by the argparser
+CHAPTER_REVISION_WRITER_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Note this value is overridden by the argparser
+REVISION_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Note this value is overridden by the argparser
+EVAL_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Note this value is overridden by the argparser
+INFO_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Note this value is overridden by the argparser
+SCRUB_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Note this value is overridden by the argparser
+CHECKER_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"  # Model used to check results
+TRANSLATOR_MODEL = "ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"
 
-OLLAMA_CTX = 8192
+# OLLAMA_CTX = 8192
+OLLAMA_CTX = 16384
 
-OLLAMA_HOST = "10.23.82.116:11434"
+# OLLAMA_HOST = "10.23.147.239:11434"
+OLLAMA_HOST = "10.23.147.239:11434"
+
 
 SEED = 12  # Note this value is overridden by the argparser
 
-TRANSLATE_LANGUAGE = "Indonesian"  # If the user wants to translate, this'll be changed from empty to a language e.g 'French' or 'Russian'
-TRANSLATE_PROMPT_LANGUAGE = "Indonesian"  # If the user wants to translate their prompt, this'll be changed from empty to a language e.g 'French' or 'Russian'
+# TRANSLATE_LANGUAGE = "Indonesian"  # If the user wants to translate, this'll be changed from empty to a language e.g 'French' or 'Russian'
+# TRANSLATE_PROMPT_LANGUAGE = "Indonesian"  # If the user wants to translate their prompt, this'll be changed from empty to a language e.g 'French' or 'Russian'
+
+TRANSLATE_LANGUAGE = ""  # If the user wants to translate, this'll be changed from empty to a language e.g 'French' or 'Russian'
+TRANSLATE_PROMPT_LANGUAGE = ""  # If the user wants to translate their prompt, this'll be changed from empty to a language e.g 'French' or 'Russian'
 
 OUTLINE_QUALITY = 87  # Note this value is overridden by the argparser
 OUTLINE_MIN_REVISIONS = 1  # Note this value is overridden by the argparser
@@ -84,5 +70,5 @@ MAX_JSON_RETRIES = 5  # Jumlah percobaan ulang maksimum untuk SafeGenerateJSON
 MAX_TEXT_RETRIES = 5  # Jumlah percobaan ulang maksimum untuk SafeGenerateText
 
 
-"ollama://gemma3:12b@10.23.147.239"
+"ollama://mychen76/gemma3_cline_roocode_qat:12b@10.23.147.239"
 "google://gemini-1.5-pro"
